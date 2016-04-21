1bf15867686d83036c64033b2b68deb8e8e05415
pinxli <pinkyt@agtechlabs.com>
2016-04-21 14:14:06 +0800

    AAP-99 Add Zip,State,City,Country Code in API Response

--- agtechapi/country/models.py
+++ agtechapi/country/models.py
@@ -9,6 +9,7 @@ class Country(models.Model):
 	country_iso2		= models.CharField(max_length=2)
 	country_iso3		= models.CharField(max_length=3)
 	country_numcode		= models.CharField(max_length=8)
+	country_dial_code	= models.CharField(max_length=8)
 	country_visible		= models.BooleanField(default=False)
 	pass
 
@@ -22,8 +23,23 @@ class State(models.Model):
 	id				= models.AutoField(primary_key=True)
 	state_name 		= models.CharField(max_length=30)
 	state_code 		= models.CharField(max_length=5)
+	# city			= models.ForeignKey(City, related_name="city", null=True, blank=True)
 	country			= models.ForeignKey(Country, related_name="state")
 	pass
 
 	def __unicode__(self):
-		return '%s - %s' % (self.state_name, self.state_code)
+		return '%s-%s' % (self.state_name, self.state_code)
+
+class City(models.Model):
+	id				= models.AutoField(primary_key=True)
+	city_name 		= models.CharField(max_length=30)
+	city_county		= models.CharField(max_length=30)
+	city_zipcode	= models.CharField(max_length=30)
+	state			= models.ForeignKey(State, related_name="city")
+	pass
+
+	class Meta:
+		ordering = ['city_name']
+
+	def __unicode__(self):
+		return '%s-%s-%s' % (self.city_name.replace('\'', ''), self.city_county.replace('\'', ''), self.city_zipcode.replace('\'', ''))
--- agtechapi/country/serializers.py
+++ agtechapi/country/serializers.py
@@ -3,10 +3,27 @@ from rest_framework import serializers
 from country.models import *
 import json
 
+class CitySerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = City
+		fields = (
+			'city_name',
+			'city_zipcode'
+		)	
+
 class StateSerializer(serializers.ModelSerializer):
+	city = serializers.StringRelatedField(many=True)
 
 	class Meta:
 		model = State
+		fields = (
+			'id', 
+			'state_name',
+			'state_code',
+			'city'
+		)	
+	
 
 class CountrySerializer(serializers.ModelSerializer):
 	state = StateSerializer(many=True, read_only=True)
@@ -20,6 +37,7 @@ class CountrySerializer(serializers.ModelSerializer):
 			'country_iso2', 
 			'country_iso3', 
 			'country_numcode', 
+			'country_dial_code', 
 			'country_visible',
 			'state'
 		)	
